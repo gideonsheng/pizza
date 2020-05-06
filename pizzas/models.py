c9d7c216cae59a300260ac9db3cbe313719dc342
Gideon <shuofeng.sheng@gmail.com>
2020-05-05 20:50:20 -0500

    1

---
+++ pizzas/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+# Create your models here.
+
+class Pizza(models.Model):
+    text = models.CharField(max_length=200)
+    date_added = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.text
+    
+    
+class Topping(models.Model):
+    pizza = models.ForeignKey(Pizza, on_delete=models.CASCADE)
+    name = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        verbose_name_plural = "pizzas"
+
+    def __str__(self):
+        return f"{self.text[:50]}..."
